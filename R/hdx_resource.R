4c4b59540425fc9af6400ae666022f49126a6991
Ahmadou Dicko <ahmadou.dicko@gmail.com>
2018-02-12 23:15:11 +0000

    Support for xls/xlsx part 2

--- R/hdx_resource.R
+++ R/hdx_resource.R
@@ -90,9 +90,9 @@ Resource <- R6::R6Class(
           check4X("readr")
           readr::read_csv(path, comment = "#")
         },
-        excel = read_sheet(path, sheet),
-        xlsx = read_sheet(path, sheet),
-        xls = read_sheet(path, sheet),
+        excel = read_sheet(path, sheet, format = "xlsx"),
+        xlsx = read_sheet(path, sheet, format = "xlsx"),
+        xls = read_sheet(path, sheet, format = "xls"),
         json = {
           check4X("jsonlite")
           jsonlite::fromJSON(path, simplifyVector = simplifyVector)
