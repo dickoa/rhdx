1568932766744c9db3f07bbd5570989831f73301
Ahmadou Dicko <ahmadou.dicko@gmail.com>
2018-02-15 20:09:07 +0000

    Add support for Location

--- R/hdx_location.R
+++ R/hdx_location.R
@@ -1,4 +1,4 @@
-#' Create and manipulate HDX Organization
+#' Create and manipulate HDX Location
 #'
 #' R6 objects are essentially environments, structured in a way that makes them
 #' look like an object in a more typical object-oriented language than R. They
@@ -43,12 +43,12 @@
 #' @examples
 #' # ---------------------------------------------------------
 #' Configuration$create(hdx_site = "demo")
-#' org <- Organization$read_from_hdx("ocha-mali", include_dataset = TRUE)
+#' org <- Location$read_from_hdx("ocha-mali", include_dataset = TRUE)
 #' org
 #' 
 #' @export
-Organization <- R6::R6Class(
-  "Organization",
+Location <- R6::R6Class(
+  "Location",
   private = list(
     configuration = NULL
   ),
@@ -64,27 +64,21 @@ Organization <- R6::R6Class(
       initial_data <- nc(initial_data)
       self$data <- initial_data
     },
-    update_from_yaml = function(hdx_organization_static_yaml) {
-      self$data <- yaml::yaml.load_file(hdx_organization_static_yaml)
+    update_from_yaml = function(hdx_location_static_yaml) {
+      self$data <- yaml::yaml.load_file(hdx_location_static_yaml)
     },
-    update_from_json = function(hdx_organization_static_json) {
-      self$data <- jsonlite::fromJSON(hdx_organization_static_json, simplifyVector = TRUE)
+    update_from_json = function(hdx_location_static_json) {
+      self$data <- jsonlite::fromJSON(hdx_location_static_json, simplifyVector = TRUE)
     },
-    read_from_hdx = function(identifier = NULL, include_datasets = FALSE, configuration = NULL, ...) {
-      if (is.null(configuration))
-        configuration <- private$configuration
-      res <- configuration$call_remoteclient("organization_show", list(id = identifier, include_datasets = include_datasets, ...))
-      Organization$new(initial_data = res$result, configuration = configuration)
-    },
-    get_all_organization_names = function(sort = "name asc", configuration = NULL, ...) {
+    list = function(sort = "name asc", configuration = NULL, ...) {
       if (!sort %in% c("name asc", "name", "package_count", "title")) stop("You can just sort by the following parameters `name asc`, `name`, `package_count` or `title`")
       if (is.null(configuration))
         configuration <- private$configuration
-      res <- configuration$call_remoteclient("organization_list", list(sort = sort, ...))
+      res <- configuration$call_remoteclient("location_list", list(sort = sort, ...))
       unlist(res$result)
     },
     get_datasets = function() {
-      if (!"packages" %in% names(self$data)) stop("No datasets available, please run again Organization$read_from_hdx with `include_dataset = TRUE` and try again!")
+      if (!"packages" %in% names(self$data)) stop("No datasets available, please run again Location$read_from_hdx with `include_dataset = TRUE` and try again!")
       list_of_ds <- lapply(self$data$packages, function(x) Dataset$new(initial_data = x))
       list_of_ds
     },
@@ -92,7 +86,7 @@ Organization <- R6::R6Class(
       self$data
     },
     print = function(x, ...) {
-    cat(paste0("<HDX Organization> ", self$data$id), "\n")
+    cat(paste0("<HDX Location> ", self$data$id), "\n")
     cat("  Name: ", self$data$name, "\n", sep = "")
     cat("  Description: ", self$data$description, "\n", sep = "")
     invisible(self)
@@ -100,56 +94,56 @@ Organization <- R6::R6Class(
   )
 )
 
-#' @aliases Organization
-Organization$read_from_hdx <- function(identifier = NULL, include_datasets = FALSE, configuration = NULL, ...) {
-  org <- Organization$new()
+#' @aliases Location
+Location$read_from_hdx <- function(identifier = NULL, include_datasets = FALSE, configuration = NULL, ...) {
+  org <- Location$new()
   org$read_from_hdx(identifier = identifier, include_datasets = include_datasets, configuration = configuration, ...)
 }
 
-#' @aliases Organization
-Organization$search_in_hdx <- function(query = "*:*", configuration = NULL, ...) {
-  org <- Organization$new()
+#' @aliases Location
+Location$search_in_hdx <- function(query = "*:*", configuration = NULL, ...) {
+  org <- Location$new()
   org$search_in_hdx(query = query, configuration = configuration, ...)
 }
 
-#' @aliases Organization
-Organization$get_all_organization_names <- function(sort = "name asc", configuration = NULL, ...) {
-  org <- Organization$new()
-  org$get_all_organization_names(sort = sort, configuration = configuration, ...)
+#' @aliases Location
+Location$get_all_location_names <- function(sort = "name asc", configuration = NULL, ...) {
+  org <- Location$new()
+  org$get_all_location_names(sort = sort, configuration = configuration, ...)
 }
 
-#' @aliases Organization
-Organization$get_datasets <- function(identifier = NULL, configuration = NULL, ...) {
-  org <- Organization$new()
+#' @aliases Location
+Location$get_datasets <- function(identifier = NULL, configuration = NULL, ...) {
+  org <- Location$new()
   res <- org$read_from_hdx(identifier = identifier, include_datasets = TRUE, configuration = configuration, ...)
   res$get_datasets()
 }
 
  
 #' @export
-#' @aliases Organization 
-as_tibble.Organization <- function(x, ...) {
+#' @aliases Location 
+as_tibble.Location <- function(x, ...) {
   df <- tibble::data_frame(
-    organization_id = x$data$id,
-    organization_name = x$data$name)
-  df$organization <- list(x)
+    location_id = x$data$id,
+    location_name = x$data$name)
+  df$location <- list(x)
   df
 }
 
 
 #' @export
-#' @aliases Organization 
-as.data.frame.Organization <- function(x, ...) {
+#' @aliases Location 
+as.data.frame.Location <- function(x, ...) {
   df <- data.frame(
-    organization_id = x$data$id,
-    organization_name = x$data$name,
+    location_id = x$data$id,
+    location_name = x$data$name,
     stringsAsFactors = FALSE)
-  df$organization <- list(x)
+  df$location <- list(x)
   df
 }
 
 #' @export
-#' @aliases Organization 
-as.list.Organization <- function(x) {
+#' @aliases Location 
+as.list.Location <- function(x) {
   x$as_list()
 }
