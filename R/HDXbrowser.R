e278f6be9389052a1ff790287115c080db376d0f
Ahmadou Dicko <ahmadou.dicko@gmail.com>
2018-02-25 20:09:10 +0000

    Add shiny apps

--- R/HDXbrowser.R
+++ R/HDXbrowser.R
@@ -1,7 +1,2 @@
-searchHDX <- function() {
-  ui <- miniPage()
-  server <- function(input, output, session) {
-  }
-   viewer <- dialogViewer("Search HDX", width = 1000, height = 800)
-  runGadget(ui, server, viewer = viewer)
-}
+HDXbrowser <- function()
+  shiny::shinyAppDir(system.file("shiny/HDXbrowser", package = "rhdx"))
